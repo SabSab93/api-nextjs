19838e2094fbb8205d7f56d86ac1e4fca59756f4
Sabrina <sabrinah_38@hotmail.fr>
2025-02-04 11:01:43 +0100

    base project

---
+++ src/app/api/toto/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import {PrismaClient}from "prisma/client";
+
+const prisma = new PrismaClient()
+
+export const GET =async ()=> {
+    const users = await prisma.user.findMany({})
+    
+}
+
+
+// export const GET = async (req: NextRequest)=> {
+//     return NextResponse.json({message:"Hello World !"})
+// };
+
+export const POST = async (request: NextRequest)=> {
+    const body = await request.json();
+    const name = body.name;
+    // ou bien en simplifiant const {name}= body;
+    // ou en plus simplifier sup la ligne 8 et 9 directement const {name} = await request.json();
+    return NextResponse.json({message:`Hello, ${name} !`})
+};
+
+
+
+
